47914a866de0fc7af191d43d851639479435f72f
Phoenix--Project <thephoenixproject208@gmail.com>
2019-05-19 12:49:35 +0100

    Requirements are working now

--- commands/purge.js
+++ commands/purge.js
@@ -1,18 +1,4 @@
-const Discord = require("discord.js");
-const config = require("./config.json");
-const utils = require('./utilities/utils.js');
-const base = require('./utilities/basebot.js');
-const dscp = require('./utilities/dscpcmds.js');
-const autow = require('./utilities/autowarning.js');
-const mbot = require('./utilities/modbot.js');
-const stats = require('./utilities/stattrack.js');
-const moment = require("moment");
-const shell = require('shelljs');
-const fs = require("fs");
-const mysql = require("mysql");
-const { exec } = require("child_process");
-const { inspect } = require('util');
-const SteamAPI = require('steamapi');
+const utils = require('../utilities/utils.js');
 const bot = require('../CorruptionBot.js')
 
 async function DeletionHandler(message, args, cmdused) {
--- commands/warn.js
+++ commands/warn.js
@@ -1,19 +1,6 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
-const utils = require('./utilities/utils.js');
-const base = require('./utilities/basebot.js');
-const dscp = require('./utilities/dscpcmds.js');
-const autow = require('./utilities/autowarning.js');
-const mbot = require('./utilities/modbot.js');
-const stats = require('./utilities/stattrack.js');
+const utils = require('../utilities/utils.js');
 const moment = require("moment");
-const shell = require('shelljs');
-const fs = require("fs");
-const mysql = require("mysql");
-const { exec } = require("child_process");
-const { inspect } = require('util');
-const SteamAPI = require('steamapi');
-const bot = require('../CorruptionBot.js')
 
 /*
   Sort out a better warning system that includes automatic removal of the warned role after 2 weeks.
